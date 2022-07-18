b2cacfe7df9a3b9a59e8d69f7d3da78868dd6e61
Sampada Huria <sampadahuria123@gmail.com>
2022-07-18 10:31:13 +0530

    Files upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+       This is an about component
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Incidunt distinctio omnis illum corrupti enim dolorum consectetur quo aperiam pariatur, aliquid laudantium temporibus, labore accusamus dicta tempora, fugit odit aut. 
+         Unde eos quisquam cupiditate! Enim.</p> 
+    </div>
+  )
+}
+
+ 
